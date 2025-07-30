2c551172c3e0ed9549b9548424c973577a9554d1
sanskriti480 <sanskriti.ten@gmail.com>
2025-07-30 14:58:48 +0530

    Create Exp 7 Printing array elements.cpp

---
+++ Exp 7 Printing array elements.cpp
@@ -0,0 +1,20 @@
+//Sanskriti Khairnar
+//PRN-24070123054
+//Batch-A2
+//Experiment 7
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {4,8,12,16,20};
+    for(int i=0 ; i<5 ; i++){
+        cout<<arr[i]<<"\t";
+    }
+    
+    return 0;
+}
+
+//Output
+
+// 4	8	12	16	20
