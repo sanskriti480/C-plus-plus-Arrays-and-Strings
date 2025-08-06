ec5ac3f24ac4f28a7a692300fdf4e516db0c76f4
sanskriti480 <sanskriti.ten@gmail.com>
2025-08-06 15:13:47 +0530

    Create Exp 7 Concatenation of string.cpp

---
+++ Exp 7 Concatenation of string.cpp
@@ -0,0 +1,19 @@
+//Sanskriti Khairnar
+//PRN-24070123054
+//Batch-A2
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char str1[] = "Sanskriti";
+    char str2[] = "Khairnar";
+    cout<<str1<<" "<<str2;
+
+    return 0;
+}
+
+//Outputs
+
+//Sanskriti Khairnar
