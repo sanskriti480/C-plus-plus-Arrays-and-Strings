953b57e44f3883eed5e5f10f8843494c98fe7978
sanskriti480 <sanskriti.ten@gmail.com>
2025-08-10 23:23:27 +0530

    Create Exp 7 Printing a string.cpp

---
+++ Exp 7 Printing a string.cpp
@@ -0,0 +1,25 @@
+//Sanskriti Khairnar
+//PRN-24070123054
+//Batch-A2
+//Experiment 7
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char str1[4] = "C++";
+    char str2[] = {'C' , '+' , '+' , '\0'};
+    char str3[4] = {'C' , '+' , '+' , '\0'};
+    string str4 = "C++";
+    cout<<str1<<endl<<str2<<endl<<str3<<endl<<str4<<endl;
+
+    return 0;
+}
+
+//Output
+
+//C++
+//C++
+//C++
+//C++
